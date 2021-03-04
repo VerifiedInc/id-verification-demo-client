9b04e6bea4c5a9a1936538fa06ab651c94991449
Jacob Singer <jacobleesinger@gmail.com>
2021-03-04 15:50:30 -0800

    [LogoutTab] give LogoutTab cursor: pointer (#5)

--- src/components/Header/LogoutTab.tsx
+++ src/components/Header/LogoutTab.tsx
@@ -3,11 +3,13 @@ import { FC } from 'react';
 import { useLogout } from '../../hooks/useLogout';
 import Tab from './Tab';
 
+import './LogoutTab.css';
+
 const LogoutTab: FC = () => {
   const logout = useLogout();
 
   return (
-    <Tab><div onClick={logout}>LOGOUT</div></Tab>
+    <Tab className='logout-tab'><div onClick={logout}>LOGOUT</div></Tab>
   );
 };
 
