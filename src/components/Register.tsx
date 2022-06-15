f609e8dafa8cd672819be39a7ea456ca6ee519d6
jacobleesinger <jacobleesinger@gmail.com>
2022-06-14 19:01:56 -0700

    [Register] automatically call prefill if vfp param is present

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -2,7 +2,8 @@ import {
   ChangeEventHandler,
   FC,
   MouseEventHandler,
-  useState
+  useState,
+  useEffect
 } from 'react';
 
 import InputGroup from './Form/InputGroup';
@@ -15,6 +16,7 @@ import Italic from './Layout/Italic';
 import { backendClient } from '../feathers';
 import { config } from '../config';
 import { getFakeDob } from '../utils';
+import { useSearchParams } from 'react-router-dom';
 
 // types for global variables added by the hyperverge sdk
 declare global {
@@ -67,8 +69,52 @@ const makeHandler = (callback: (data: KYCData) => void) => (HyperKycResult: any)
   }
 };
 
+const handlePreFill = async (verificationFingerprint: string, mobileNumber: string, dob?: string | null) => {
+  console.log('\n\nhandlePrefill');
+  const fakeDob = getFakeDob(mobileNumber);
+
+  const authPathService = backendClient.service('getAuthPath');
+  // TODO add auth with backend service
+  const responseAuthPath = await authPathService.create({
+    verificationFingerprint
+  });
+  // TODO ensure success response
+
+  const eligibilityService = backendClient.service('eligibility');
+  // TODO add auth with backend service
+  const responseEligibility = await eligibilityService.create({
+    phoneNumber: mobileNumber,
+    minTrustScore: 500
+  });
+
+  const identityService = backendClient.service('identity');
+  // TODO add auth with backend service
+  const responseIdentity = await identityService.create({
+    dob: fakeDob,
+    // dob: kyc.data.dateOfBirth, // NOTE: can't actually do this because this is after the sms link soo... need to get from query params like below
+    // dob, // TODO the dob query param needs to be used, but can't because staging data is not what's on my document
+    phoneNumber: mobileNumber
+  });
+
+  const { userCode, issuerDid } = responseIdentity;
+
+  // TODO check 200 success response from backend
+  // redirect to wallet client with query params for user to create DID
+  window.location.href = `${config.walletClientUrl}/authenticate?userCode=${userCode}&issuer=${issuerDid}`;
+};
+
 const Register: FC = () => {
   const [phone, setPhone] = useState('');
+  const [searchParams] = useSearchParams();
+  const vfpParam = searchParams.get('vfp');
+  const dobParam = searchParams.get('dob');
+  const phoneParam = searchParams.get('phone');
+
+  useEffect(() => {
+    if (vfpParam && phoneParam) {
+      handlePreFill(vfpParam, phoneParam, dobParam);
+    }
+  }, [vfpParam, dobParam, phoneParam]);
 
   const handlePhoneChange: ChangeEventHandler<HTMLInputElement> = (e) => {
     setPhone(e.target.value);
@@ -120,51 +166,6 @@ const Register: FC = () => {
     window.HyperKYCModule.launch(hyperKycConfig, handler);
   };
 
-  const handlePreFill2: MouseEventHandler = async (e) => {
-    e.preventDefault();
-
-    const urlQueryParams: string = window.location.search;
-    const queryParams = new URLSearchParams(urlQueryParams);
-
-    const verificationFingerprint = queryParams.get('vfp');
-    const dob = queryParams.get('dob');
-    const qPhone = queryParams.get('phone');
-
-    // use the phone via the query params from the sms redirect from prove...
-    // TODO this value should not be editable is phone is in the query param
-    const mobileNumber = phone || qPhone || '4044327575';
-    const fakeDob = getFakeDob(mobileNumber);
-
-    const authPathService = backendClient.service('getAuthPath');
-    // TODO add auth with backend service
-    const responseAuthPath = await authPathService.create({
-      verificationFingerprint
-    });
-    // TODO ensure success response
-
-    const eligibilityService = backendClient.service('eligibility');
-    // TODO add auth with backend service
-    const responseEligibility = await eligibilityService.create({
-      phoneNumber: mobileNumber,
-      minTrustScore: 500
-    });
-
-    const identityService = backendClient.service('identity');
-    // TODO add auth with backend service
-    const responseIdentity = await identityService.create({
-      dob: fakeDob,
-      // dob: kyc.data.dateOfBirth, // NOTE: can't actually do this because this is after the sms link soo... need to get from query params like below
-      // dob, // TODO the dob query param needs to be used, but can't because staging data is not what's on my document
-      phoneNumber: mobileNumber
-    });
-
-    const { userCode, issuerDid } = responseIdentity;
-
-    // TODO check 200 success response from backend
-    // redirect to wallet client with query params for user to create DID
-    window.location.href = `${config.walletClientUrl}/authenticate?userCode=${userCode}&issuer=${issuerDid}`;
-  };
-
   return (
     <div className='register'>
       <h1>Register</h1>
@@ -187,7 +188,6 @@ const Register: FC = () => {
           explainerText='Enter this to facilitate identity verification via SMS.'
         />
         <SubmitButton handleSubmit={handleDocScan}><BoldFont>Documentation Scan</BoldFont></SubmitButton>&nbsp;
-        <SubmitButton handleSubmit={handlePreFill2}><BoldFont>PreFill Step 2 From Mobile</BoldFont></SubmitButton>
       </form>
       <div>
         By creating an account you agree to our <a href='https://www.unumid.co/legal-materials/terms-of-service'>terms of service</a> and <a href='https://www.unumid.co/legal-materials/privacy-policy'>privacy policy</a>.
