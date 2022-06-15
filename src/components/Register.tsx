d98077440d5fe12a53a38d01c972af4f3c03419a
Jacob Singer <jacobleesinger@gmail.com>
2022-06-14 19:13:56 -0700

    Improvements (#8)
    * [Register] rm unused stuff
    
    * [index.html, Register] remove localstorage jank
    
    * [Register] get hyperverge access token on load
    
    * [Register] get hyperverge token on document scan click
    
    * [Register] send prove sms when hyperverge completes
    
    * [Register] cleanup
    
    * [Register] automatically call prefill if vfp param is present

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,91 +1,129 @@
-import { ChangeEventHandler, FC, MouseEventHandler, useState } from 'react';
-import { Link, useSearchParams } from 'react-router-dom';
-import validator from 'validator';
+import {
+  ChangeEventHandler,
+  FC,
+  MouseEventHandler,
+  useState,
+  useEffect
+} from 'react';
 
 import InputGroup from './Form/InputGroup';
 import SubmitButton from './Form/SubmitButton';
-import ErrorMessage from './Form/ErrorMessage';
 import BoldFont from './Layout/BoldFont';
-
-import { useActionCreators } from '../hooks/useActionCreators';
-import { useTypedSelector } from '../hooks/useTypedSelector';
 
 import './Register.css';
 import Italic from './Layout/Italic';
 
-import axios from 'axios';
 import { backendClient } from '../feathers';
 import { config } from '../config';
 import { getFakeDob } from '../utils';
+import { useSearchParams } from 'react-router-dom';
+
+// types for global variables added by the hyperverge sdk
+declare global {
+  interface Window {
+    HyperKycConfig?: any;
+    Face?: any;
+    Document?: any;
+    HyperKYCModule?: any;
+  }
+}
+
+interface KYCData {
+  address: string;
+  age: string;
+  dob: string;
+  gender: string;
+  fullName: string;
+  idType: string;
+}
+
+const makeHandler = (callback: (data: KYCData) => void) => (HyperKycResult: any) => {
+  if (HyperKycResult.Cancelled) {
+    // user cancelled
+    console.log('hyperverge cancelled', HyperKycResult);
+  } else if (HyperKycResult.Failure) {
+    // fail
+    console.log('hyperverge failed', HyperKycResult);
+  } else if (HyperKycResult.Success) {
+    // success
+    console.log('hyperverge success', HyperKycResult);
+    const { address, age, dateOfBirth, fullName, idType, gender } = HyperKycResult.Success.data.docListData[0].responseResult.result.details[0].fieldsExtracted;
+    const proveDob = dateOfBirth.value.split('-');
+    const hold = proveDob[2];
+    proveDob[2] = proveDob[1];
+    proveDob[1] = proveDob[0];
+    proveDob[0] = hold;
+    const dob = proveDob.join('-');
+
+    // eslint-disable-next-line node/no-callback-literal
+    callback({
+      address,
+      age,
+      dob,
+      fullName,
+      idType,
+      gender
+    });
+
+    console.log('success');
+  }
+};
+
+const handlePreFill = async (verificationFingerprint: string, mobileNumber: string, dob?: string | null) => {
+  console.log('\n\nhandlePrefill');
+  const fakeDob = getFakeDob(mobileNumber);
+
+  const authPathService = backendClient.service('getAuthPath');
+  // TODO add auth with backend service
+  const responseAuthPath = await authPathService.create({
+    verificationFingerprint
+  });
+  // TODO ensure success response
+
+  const eligibilityService = backendClient.service('eligibility');
+  // TODO add auth with backend service
+  const responseEligibility = await eligibilityService.create({
+    phoneNumber: mobileNumber,
+    minTrustScore: 500
+  });
+
+  const identityService = backendClient.service('identity');
+  // TODO add auth with backend service
+  const responseIdentity = await identityService.create({
+    dob: fakeDob,
+    // dob: kyc.data.dateOfBirth, // NOTE: can't actually do this because this is after the sms link soo... need to get from query params like below
+    // dob, // TODO the dob query param needs to be used, but can't because staging data is not what's on my document
+    phoneNumber: mobileNumber
+  });
+
+  const { userCode, issuerDid } = responseIdentity;
+
+  // TODO check 200 success response from backend
+  // redirect to wallet client with query params for user to create DID
+  window.location.href = `${config.walletClientUrl}/authenticate?userCode=${userCode}&issuer=${issuerDid}`;
+};
 
 const Register: FC = () => {
-  const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
-  const [firstName, setFirstName] = useState('');
-  const [password] = useState('password');
-  const [formErrorMessage, setFormErrorMessage] = useState('');
+  const [searchParams] = useSearchParams();
+  const vfpParam = searchParams.get('vfp');
+  const dobParam = searchParams.get('dob');
+  const phoneParam = searchParams.get('phone');
 
-  const { createUser } = useActionCreators();
-  const { loggedInUser, error: loginError } = useTypedSelector(state => state.auth);
-  const { error: userError } = useTypedSelector(state => state.user);
-
-  if (userError && !formErrorMessage) {
-    console.error('error creating user account', userError);
-    setFormErrorMessage('Error creating account');
-  }
-
-  if (loginError && !formErrorMessage) {
-    console.error('error logging in', loginError);
-    setFormErrorMessage('Error logging in');
-  }
-
-  // const urlQueryParams: string = window.location.search;
-  // const queryParams = new URLSearchParams(urlQueryParams);
-  // const dob = queryParams.get('dob');
-  // setEmail(dob as string); // TODO update: email is being used for dob... this should be fixed.
-
-  const handleEmailChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-    setEmail(e.target.value);
-  };
+  useEffect(() => {
+    if (vfpParam && phoneParam) {
+      handlePreFill(vfpParam, phoneParam, dobParam);
+    }
+  }, [vfpParam, dobParam, phoneParam]);
 
   const handlePhoneChange: ChangeEventHandler<HTMLInputElement> = (e) => {
     setPhone(e.target.value);
   };
 
-  const handleFirstNameChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-    setFirstName(e.target.value);
-  };
-
-  const handleStart: MouseEventHandler = async (e) => {
-    e.preventDefault();
-
-    debugger;
-    // get hv auth token
-    const hyperVergeAuthService = backendClient.service('hyperVergeAuth');
-    // TODO add auth with backend service
-    const responseAuth = await hyperVergeAuthService.create({});
-    localStorage.setItem('authToken', responseAuth.result.token);
-    debugger;
-
-    // // kick off prove sms
-    // // TODO add auth with backend
-    // const proveAuthUrlService = backendClient.service('getAuthUrl');
-    // const responseAuthUrl = await proveAuthUrlService.create({
-    //   mobileNumber: phone
-    // });
-    // // TODO ensure success response
-  };
-
-  const handleDocScan: MouseEventHandler = (e) => {
-    // e.preventDefault();
-    localStorage.setItem('doKyc', 'true');
-  };
-
-  const mobileNumber = phone || '4044327575'; // TODO remove, added for easier testing
-
-  const handlePreFill1: MouseEventHandler = async (e) => {
-    e.preventDefault();
-
+  /**
+   * calls prove auth service to send an auth url via sms
+   */
+  const sendProveSms = async (dob?: string) => {
     /**
      * NOTE: Maybe want to just point blank ask for the DOB in the form input and compare against the hyper verge doc scan info prior to kicking off the prove prefill flow...?
      */
@@ -93,67 +131,39 @@ const Register: FC = () => {
     // kick off prove sms
     // TODO add auth with backend
     const proveAuthUrlService = backendClient.service('getAuthUrl');
-    const kyc = JSON.parse(localStorage.getItem('kycInfo') as string);
 
-    debugger;
     const responseAuthUrl = await proveAuthUrlService.create({
-      mobileNumber,
-      dob: kyc.data.dateOfBirth // from the hv doc scan... will present the query params of the resultant sms link.
+      mobileNumber: phone,
+      dob // from the hv doc scan... will present the query params of the resultant sms link.
     });
     // TODO ensure success response
   };
 
-  const handlePreFill2: MouseEventHandler = async (e) => {
+  const handleDocScan: MouseEventHandler = async (e) => {
     e.preventDefault();
-    const kyc = JSON.parse(localStorage.getItem('kycInfo') as string);
-    // console.log(kyc.data);
-    debugger;
 
-    const urlQueryParams: string = window.location.search;
-    const queryParams = new URLSearchParams(urlQueryParams);
-
-    const verificationFingerprint = queryParams.get('vfp');
-    const dob = queryParams.get('dob');
-    const qPhone = queryParams.get('phone');
-
-    // use the phone via the query params from the sms redirect from prove...
-    // TODO this value should not be editable is phone is in the query param
-    const mobileNumber = phone || qPhone || '4044327575';
-    const fakeDob = getFakeDob(mobileNumber);
-
-    debugger;
-    const authPathService = backendClient.service('getAuthPath');
+    // get hyperverge access token
+    const hyperVergeAuthService = backendClient.service('hyperVergeAuth');
     // TODO add auth with backend service
-    const responseAuthPath = await authPathService.create({
-      verificationFingerprint
-    });
-    debugger;
-    // TODO ensure success response
+    const responseAuth = await hyperVergeAuthService.create({});
+    const accessToken = responseAuth.result.token;
 
-    const eligibilityService = backendClient.service('eligibility');
-    // TODO add auth with backend service
-    const responseEligibility = await eligibilityService.create({
-      phoneNumber: mobileNumber,
-      minTrustScore: 500
-    });
-    debugger;
+    // hyperverge document scan setup
+    const defaultDocumentId = 'dl';
+    const defaultCountryId = 'usa';
+    const transactionId = '1';
+    const document2 = new window.Document(true, defaultCountryId, defaultDocumentId);
+    const face = new window.Face();
+    const workflow = [document2, face];
+    const hyperKycConfig = new window.HyperKycConfig(accessToken, workflow, transactionId, defaultCountryId);
 
-    const identityService = backendClient.service('identity');
-    // TODO add auth with backend service
-    const responseIdentity = await identityService.create({
-      dob: fakeDob,
-      // dob: kyc.data.dateOfBirth, // NOTE: can't actually do this because this is after the sms link soo... need to get from query params like below
-      // dob, // TODO the dob query param needs to be used, but can't because staging data is not what's on my document
-      phoneNumber: mobileNumber
-    });
+    const callback = (data: KYCData) => {
+      sendProveSms(data.dob);
+    };
 
-    const { userCode, issuerDid } = responseIdentity;
+    const handler = makeHandler(callback);
 
-    debugger;
-    // TODO check 200 success response from backend
-    // redirect to wallet client with query params for user to create DID
-    window.location.href = `${config.walletClientUrl}/authenticate?userCode=${userCode}&issuer=${issuerDid}`;
-    debugger;
+    window.HyperKYCModule.launch(hyperKycConfig, handler);
   };
 
   return (
@@ -165,56 +175,23 @@ const Register: FC = () => {
         This shows that Unum ID can be used <Italic>in conjunction with</Italic> an existing identity verification provider.
         We can also fully replace that system, using the technologies used here.
       </p>
-      {
-        loggedInUser
-          ? (
-            <>
-              <h2>2. Install ACME App</h2>
-              <p>
-                Great, you're registered! Now, install the ACME app for <a href='https://apps.apple.com/us/app/acme-unum-id/id1557576599'>iOS</a> or <a href='https://play.google.com/store/apps/details?id=org.unumid.acmedemokotlin.sandbox&hl=en_GB&gl=US'>Android</a>.
-              </p>
-              <p>
-                You can get there by clicking the links above, scanning the QR Code below, or searching "ACME Unum ID" on the app stores.
-                The app will ask you for your permission to send push notifications.&nbsp;
-                <BoldFont>This is optional, </BoldFont>
-                but it will allow you to see how users can authenticate with push notifications.
-              </p>
-              <p>
-                When you&apos;re logged in to the ACME app,
-                click the button below to continue the demo.
-              </p>
-              <Link to='/'><button type='button'>Continue</button></Link>
-            </>
-            )
-          : (
-            <>
-              <form>
-                <h2>1. Create Account</h2>
-                <InputGroup
-                  required
-                  labelText='Phone'
-                  inputId='phone'
-                  type='text'
-                  onChange={handlePhoneChange}
-                  value={phone}
-                  explainerBoldText='Use your real mobile number:'
-                  explainerText='Enter this to facilitate identity verification via SMS.'
-                />
-                <SubmitButton handleSubmit={handleStart}><BoldFont>Start</BoldFont></SubmitButton>&nbsp;
-                <SubmitButton handleSubmit={handleDocScan}><BoldFont>Documentation Scan</BoldFont></SubmitButton>&nbsp;
-                <SubmitButton handleSubmit={handlePreFill1}><BoldFont>PreFill Step 1 From Desktop</BoldFont></SubmitButton>&nbsp;
-                <SubmitButton handleSubmit={handlePreFill2}><BoldFont>PreFill Step 2 From Mobile</BoldFont></SubmitButton>
-                <ErrorMessage>{formErrorMessage}</ErrorMessage>
-              </form>
-              <div>
-                By creating an account you agree to our <a href='https://www.unumid.co/legal-materials/terms-of-service'>terms of service</a> and <a href='https://www.unumid.co/legal-materials/privacy-policy'>privacy policy</a>.
-              </div>
-              <div className='login-link'>
-                Already registered?&nbsp;<Link to='/login'>Log in here.</Link>
-              </div>
-            </>
-            )
-      }
+      <form>
+        <h2>1. Create Account</h2>
+        <InputGroup
+          required
+          labelText='Phone'
+          inputId='phone'
+          type='text'
+          onChange={handlePhoneChange}
+          value={phone}
+          explainerBoldText='Use your real mobile number:'
+          explainerText='Enter this to facilitate identity verification via SMS.'
+        />
+        <SubmitButton handleSubmit={handleDocScan}><BoldFont>Documentation Scan</BoldFont></SubmitButton>&nbsp;
+      </form>
+      <div>
+        By creating an account you agree to our <a href='https://www.unumid.co/legal-materials/terms-of-service'>terms of service</a> and <a href='https://www.unumid.co/legal-materials/privacy-policy'>privacy policy</a>.
+      </div>
     </div>
   );
 };
