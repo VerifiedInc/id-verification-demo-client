3410b0dccf0f2f0f6da3f6889f1b2fc20360355e
Ray Smets <rayjsmets@gmail.com>
2022-07-21 16:17:05 -0700

    [CI & SubjectDidAssociation] CI made more robust and SubjectDidAssociation redirect url fix to point at deeplink router instead of the wallet directly. (#17)
    * [CI] consolidated.
    
    * logging
    
    * fox
    
    * fix
    
    * fix
    
    * fix
    
    * ci
    
    * urlfix
    
    * cleanup

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -161,7 +161,9 @@ const handlePreFill = async (verificationFingerprint: string, mobileNumber: stri
 
     // TODO check 200 success response from backend
     // redirect to wallet client with query params for user to create DID
-    window.location.href = `${config.deeplinkServerUrl}/authenticate?userCode=${userCode}&issuer=${issuerDid}`;
+    debugger;
+    console.log('deeplinkurl', config.deeplinkServerUrl);
+    window.location.href = `${config.deeplinkServerUrl}/${config.holderAppUuid}/subjectDidAssociation?userCode=${userCode}&issuer=${issuerDid}`;
   } catch (e) {
     console.log('identity error', e);
     window.alert('Error interfacing with Prove Prefill service. Please try again.');
